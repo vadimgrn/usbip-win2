2f8290f4315c6e812b0dfc55acf8416f8c9ada68
vadimgrn <67631045+vadimgrn@users.noreply.github.com>
2021-07-13 17:00:11 +0300

    vhci_proto.c: refactoring of transflag
    Signed-off-by: vadimgrn <67631045+vadimgrn@users.noreply.github.com>

--- driver/vhci_ude/vhci_proto.h
+++ driver/vhci_ude/vhci_proto.h
@@ -4,9 +4,6 @@
 
 struct usbip_header;
 
-unsigned int
-transflag(unsigned int flags);
-
 void
 set_cmd_submit_usbip_header(struct usbip_header *h, unsigned long seqnum, unsigned int devid,
 			    unsigned int direct, pctx_ep_t ep, unsigned int flags, unsigned int len);
