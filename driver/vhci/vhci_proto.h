daf98ddb255dc3f0cb4aaabafa5434255d174907
vadimgrn <67631045+vadimgrn@users.noreply.github.com>
2021-12-24 12:41:19 +0200

    Use C++20 for all projects
    Signed-off-by: vadimgrn <67631045+vadimgrn@users.noreply.github.com>

--- driver/vhci/vhci_proto.h
+++ driver/vhci/vhci_proto.h
@@ -5,6 +5,10 @@
 #include <wdm.h>
 #include <usb.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 struct usbip_header;
 
 NTSTATUS set_cmd_submit_usbip_header(
@@ -12,3 +16,7 @@ NTSTATUS set_cmd_submit_usbip_header(
 	USBD_PIPE_HANDLE pipe, ULONG TransferFlags, ULONG TransferBufferLength);
 
 void set_cmd_unlink_usbip_header(struct usbip_header *h, unsigned long seqnum, unsigned int devid, unsigned long seqnum_unlink);
+
+#ifdef __cplusplus
+}
+#endif
