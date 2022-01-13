161bb2c74d849df0d56bde08c27b6dfffab30cde
vadimgrn <67631045+vadimgrn@users.noreply.github.com>
2022-01-13 10:51:45 +0200

    Version 0.7.0.0
    Signed-off-by: vadimgrn <67631045+vadimgrn@users.noreply.github.com>

--- userspace/config.h
+++ userspace/config.h
@@ -3,7 +3,7 @@
 #define _USBIP_CONFIG_WINDOWS_H
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "usbip-win 0.3.5"
+#define PACKAGE_STRING "usbip-win 0.7.0"
 
 /* numeric version number */
 #define USBIP_VERSION 0x000111
