ae06e17d0fd646b34c98789bf874991627c9ede6
vadimgrn <67631045+vadimgrn@users.noreply.github.com>
2022-01-15 14:45:30 +0200

    Remove the last goto
    Signed-off-by: vadimgrn <67631045+vadimgrn@users.noreply.github.com>

--- driver/vhci/ioctl.cpp
+++ driver/vhci/ioctl.cpp
@@ -12,30 +12,30 @@ extern "C" PAGEABLE NTSTATUS vhci_ioctl(__in DEVICE_OBJECT *devobj, __in IRP *ir
 	PAGED_CODE();
 
 	auto vdev = to_vdev(devobj);
-	auto status = STATUS_INVALID_DEVICE_REQUEST;
-
-	auto irpstack = IoGetCurrentIrpStackLocation(irp);
-	auto ioctl_code = irpstack->Parameters.DeviceIoControl.IoControlCode;
-
-	TraceCall("%!vdev_type_t!: enter irql %!irql!, %s(%#08lX)",
-			vdev->type, KeGetCurrentIrql(), dbg_ioctl_code(ioctl_code), ioctl_code);
-
-	auto buffer = irp->AssociatedIrp.SystemBuffer;
-
-	auto inlen = irpstack->Parameters.DeviceIoControl.InputBufferLength;
-	auto outlen = irpstack->Parameters.DeviceIoControl.OutputBufferLength;
 
 	if (vdev->PnPState == pnp_state::Removed) {
-		status = STATUS_NO_SUCH_DEVICE;
-		goto END;
+		TraceCall("%!vdev_type_t! -> NO_SUCH_DEVICE", vdev->type);
+		return irp_done(irp, STATUS_NO_SUCH_DEVICE);
 	}
+
+	auto irpstack = IoGetCurrentIrpStackLocation(irp);
+	auto &ioc = irpstack->Parameters.DeviceIoControl;
+
+	TraceCall("%!vdev_type_t!: enter irql %!irql!, %s(%#08lX)",
+			vdev->type, KeGetCurrentIrql(), dbg_ioctl_code(ioc.IoControlCode), ioc.IoControlCode);
+
+	auto inlen = ioc.InputBufferLength;
+	auto outlen = ioc.OutputBufferLength;
+
+	auto buffer = irp->AssociatedIrp.SystemBuffer;
+	auto status = STATUS_INVALID_DEVICE_REQUEST;
 
 	switch (vdev->type) {
 	case VDEV_VHCI:
-		status = vhci_ioctl_vhci((vhci_dev_t*)vdev, irpstack, ioctl_code, buffer, inlen, &outlen);
+		status = vhci_ioctl_vhci((vhci_dev_t*)vdev, irpstack, ioc.IoControlCode, buffer, inlen, &outlen);
 		break;
 	case VDEV_VHUB:
-		status = vhci_ioctl_vhub((vhub_dev_t*)vdev, irp, ioctl_code, buffer, inlen, &outlen);
+		status = vhci_ioctl_vhub((vhub_dev_t*)vdev, irp, ioc.IoControlCode, buffer, inlen, &outlen);
 		break;
 	default:
 		Trace(TRACE_LEVEL_WARNING, "ioctl for %!vdev_type_t! is not allowed", vdev->type);
@@ -44,7 +44,6 @@ extern "C" PAGEABLE NTSTATUS vhci_ioctl(__in DEVICE_OBJECT *devobj, __in IRP *ir
 
 	irp->IoStatus.Information = outlen;
 
-END:
 	if (status != STATUS_PENDING) {
 		irp_done(irp, status);
 	}
