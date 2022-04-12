987137379d702ebce78ad9db6d6f9a8946f657bb
Vadym Hrynchyshyn <vadimgrn@gmail.com>
2022-04-12 16:30:23 +0300

    Refactoring start_xfer
    Signed-off-by: Vadym Hrynchyshyn <vadimgrn@gmail.com>

--- userspace/lib/start_xfer.cpp
+++ userspace/lib/start_xfer.cpp
@@ -19,105 +19,97 @@
 #include "start_xfer.h"
 #include "usbip_xfer/usbip_xfer.h"
 #include "usbip_network.h"
+#include "win_handle.h"
+#include "usbip_util.h"
 
-#include <assert.h>
+#include <cassert>
 
-static BOOL write_data(HANDLE hInWrite, const void *data, DWORD len)
+namespace
 {
-	DWORD nwritten = 0;
-	BOOL res = WriteFile(hInWrite, data, len, &nwritten, nullptr);
 
-	if (res && nwritten == len) {
-		return TRUE;
-	}
-
-	dbg("failed to write handle value, len %d", len);
-	return FALSE;
-}
-
-static BOOL create_pipe(HANDLE *phRead, HANDLE *phWrite)
+BOOL write_data(HANDLE hInWrite, const void *data, DWORD len)
 {
-	SECURITY_ATTRIBUTES saAttr{sizeof(saAttr), nullptr, TRUE};
+        DWORD nwritten = 0;
+        BOOL res = WriteFile(hInWrite, data, len, &nwritten, nullptr);
 
-	if (CreatePipe(phRead, phWrite, &saAttr, 0)) {
-		return TRUE;
-	}
-
-	dbg("failed to create stdin pipe: 0x%lx", GetLastError());
-	return FALSE;
-}
-
-static bool get_usbip_xfer_path(char *path, DWORD cch)
-{
-        DWORD n = GetModuleFileName(nullptr, path, cch);
-        if (!(n > 0 && n < cch)) {
-                dbg("GetModuleFileName error %#0x", GetLastError());
-                return false;
+        if (res && nwritten == len) {
+                return TRUE;
         }
 
-        LPSTR fname = nullptr;
-        n = GetFullPathName(path, cch, path, &fname);
-        assert(n > 0 && n < cch);
-
-        strcpy_s(fname, cch - (fname - path), usbip_xfer_binary());
-        return true;
+        dbg("failed to write handle value, len %d", len);
+        return FALSE;
 }
+
+auto create_pipe()
+{
+        std::pair<usbip::Handle, usbip::Handle> p;
+
+        HANDLE hRead;
+        HANDLE hWrite;
+        SECURITY_ATTRIBUTES saAttr{ sizeof(saAttr), nullptr, TRUE };
+
+        if (CreatePipe(&hRead, &hWrite, &saAttr, 0)) {
+                p.first.reset(hRead);
+                p.second.reset(hWrite);
+        }
+
+        dbg("failed to create stdin pipe: 0x%lx", GetLastError());
+        return p;
+}
+
+} // namespace
+
 
 int start_xfer(HANDLE hdev, SOCKET sockfd, bool client)
 {
         int ret = ERR_GENERAL;
-
+        
         usbip_xfer_args args{ INVALID_HANDLE_VALUE };
         args.client = client;
 
-        HANDLE hRead;
-	HANDLE hWrite;
-	if (!create_pipe(&hRead, &hWrite)) {
-		return ERR_GENERAL;
+        auto pipe = create_pipe();
+        if (!(pipe.first && pipe.second)) {
+		return ret;
 	}
 
-	char CommandLine[MAX_PATH];
-        if (!get_usbip_xfer_path(CommandLine, ARRAYSIZE(CommandLine))) {
-                return ERR_GENERAL;
-        }
+        auto exe_path = get_module_dir() + '\\' + usbip_xfer_binary();
+        
+        char CommandLine[MAX_PATH];
+        strncpy_s(CommandLine, exe_path.c_str(), MAX_PATH);
 
 	STARTUPINFO si{sizeof(si)};
-	si.hStdInput = hRead,
+	si.hStdInput = pipe.first.get(), // read
 	si.dwFlags = STARTF_USESTDHANDLES;
 
 	PROCESS_INFORMATION pi{};
-        BOOL res = CreateProcess(nullptr, CommandLine, nullptr, nullptr, TRUE, 0, nullptr, nullptr, &si, &pi);
+        auto res = CreateProcess(nullptr, CommandLine, nullptr, nullptr, TRUE, 0, nullptr, nullptr, &si, &pi);
 	if (!res) {
-		DWORD err = GetLastError();
+		auto err = GetLastError();
 		if (err == ERROR_FILE_NOT_FOUND) {
 			ret = ERR_NOTEXIST;
 		}
 		dbg("failed to create process: 0x%lx", err);
-		goto out;
+		return ret;
 	}
+
+        usbip::Handle hProcess(pi.hProcess);
+        usbip::Handle hThread(pi.hThread);
 
 	res = DuplicateHandle(GetCurrentProcess(), hdev, pi.hProcess, &args.hdev, 0, FALSE, DUPLICATE_SAME_ACCESS);
 	if (!res) {
 		dbg("failed to dup hdev: 0x%lx", GetLastError());
-		goto out_proc;
-	}
+                return ret;
+        }
 
 	res = WSADuplicateSocketW(sockfd, pi.dwProcessId, &args.info);
 	if (res) {
 		dbg("failed to dup sockfd: %#x", WSAGetLastError());
-		goto out_proc;
-	}
+                return ret;
+        }
 
-	if (write_data(hWrite, &args, sizeof(args))) {
+	if (write_data(pipe.second.get(), &args, sizeof(args))) {
 		ret = 0;
 	}
-
-out_proc:
-	CloseHandle(pi.hProcess);
-	CloseHandle(pi.hThread);
-out:
-	CloseHandle(hRead);
-	CloseHandle(hWrite);
 
 	return ret;
 }
