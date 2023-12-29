db3870ca10c72fd062a8e77c5e331cc5864756a4
Vadym Hrynchyshyn <vadimgrn@gmail.com>
2023-12-29 10:45:52 +0200

    Overlapped I/O
    Signed-off-by: Vadym Hrynchyshyn <vadimgrn@gmail.com>

--- userspace/wusbip/wusbip.cpp
+++ userspace/wusbip/wusbip.cpp
@@ -5,11 +5,11 @@
 #include "wusbip.h"
 #include "utils.h"
 
+#include <libusbip/remote.h>
+
 #include <wx/app.h>
 #include <wx/msgdlg.h>
-
-#include <libusbip/vhci.h>
-#include <libusbip/remote.h>
+#include <wx/log.h> 
 
 namespace
 {
@@ -40,6 +40,11 @@ bool App::OnInit()
         return true;
 }
 
+void log_last_error(const char *what, DWORD msg_id = GetLastError())
+{
+        wxLogError("{}: {}", what, GetLastErrorMsg(msg_id));
+}
+
 } // namespace
 
 
@@ -68,4 +73,56 @@ void MainFrame::on_detach(wxCommandEvent&)
 void MainFrame::on_port(wxCommandEvent&)
 {
         wxMessageBox(__func__);
+}
+
+bool MainFrame::async_read()
+{
+        auto &vhci = get_vhci();
+
+        if (!ReadFileEx(vhci.get(), m_read_buf.data(), DWORD(m_read_buf.size()), &m_overlapped, on_read)) {
+                log_last_error("ReadFileEx");
+                return false;
+        }
+
+        switch (auto err = GetLastError()) {
+        case ERROR_SUCCESS:
+        case ERROR_IO_PENDING:
+                return true;
+        default:
+                log_last_error("ReadFileEx", err);
+                return false;
+        }
+}
+
+void MainFrame::on_read(DWORD errcode, DWORD /*NumberOfBytesTransfered*/, OVERLAPPED *overlapped)
+{
+        if ( errcode != ERROR_SUCCESS ) {
+                log_last_error(__func__, errcode);
+                return;
+        }
+
+        auto &self = *static_cast<MainFrame*>(overlapped->hEvent);
+
+        DWORD actual{};
+        if (auto &dev = get_vhci(); !GetOverlappedResult(dev.get(), overlapped, &actual, false)) {
+                log_last_error("GetOverlappedResult");
+                return;
+        }
+
+        assert(actual <= self.m_read_buf.size());
+
+        usbip::device_state st;
+        if (!vhci::get_device_state(st, self.m_read_buf.data(), actual)) {
+                log_last_error("vhci::get_device_state");
+                return;
+        } 
+
+        self.state_changed(st);
+        self.async_read();
+}
+
+void MainFrame::state_changed(const usbip::device_state &st)
+{
+        auto s = usbip::vhci::get_state_str(st.state);
+        SetStatusText(s);
 }
--- userspace/wusbip/wusbip.h
+++ userspace/wusbip/wusbip.h
@@ -5,15 +5,26 @@
 #pragma once
 
 #include "frame.h"
+#include <libusbip/vhci.h>
+
+#include <vector>
 
 class MainFrame : public Frame
 {
 public:
 	MainFrame() : Frame(nullptr) {}
 private:
+	OVERLAPPED m_overlapped{ 0, 0, {0}, this }; // see ReadFileEx
+	std::vector<char> m_read_buf = std::vector<char>( usbip::vhci::get_device_state_size() );
+
 	void on_exit(wxCommandEvent &event) override;
 	void on_list(wxCommandEvent &event) override;
 	void on_attach(wxCommandEvent &event) override;
 	void on_detach(wxCommandEvent &event) override;
 	void on_port(wxCommandEvent &event) override;
+
+	bool async_read();
+	static void on_read(DWORD errcode, DWORD NumberOfBytesTransfered, OVERLAPPED *overlapped);
+
+	void state_changed(const usbip::device_state &st);
 };
