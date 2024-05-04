6c11fcf6c9bb5fea75124f9d38efa510e2619353
Vadym Hrynchyshyn <vadimgrn@gmail.com>
2024-05-04 09:28:03 +0300

    v.0.9.6.3
    Signed-off-by: Vadym Hrynchyshyn <vadimgrn@gmail.com>

--- bin/di.bat
+++ bin/di.bat
@@ -1,2 +1,2 @@
 rem del /F C:\Windows\INF\setupapi.*.log
-D:\usbip-win2\x64\Debug\USBip-0.9.6.2-Debug.exe /VERYSILENT /NORESTART
+D:\usbip-win2\x64\Debug\USBip-0.9.6.3-Debug.exe /VERYSILENT /NORESTART
