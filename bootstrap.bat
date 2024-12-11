b3dbee66d72eaf52d7c32171640fbdedd9144561
Vadym Hrynchyshyn <vadimgrn@gmail.com>
2024-12-11 10:13:38 +0200

    bootstrap.bat: checkout master and pull for vcpkg
    Signed-off-by: Vadym Hrynchyshyn <vadimgrn@gmail.com>

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,3 +1,12 @@
-git.exe submodule update --init --recursive
-cd vcpkg && bootstrap-vcpkg.bat -disableMetrics
-cd ..
+@echo off
+
+set GIT=git.exe
+SET CUR_DIR=%CD%
+
+%GIT% submodule update --init --recursive
+
+cd vcpkg
+%GIT% checkout master
+%GIT% pull
+bootstrap-vcpkg.bat -disableMetrics
+cd %CUR_DIR%
