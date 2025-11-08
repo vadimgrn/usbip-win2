5b95be8b65d01585c3b11bd8b9c93c8d2a1e9c5c
Vadym Hrynchyshyn <vadimgrn@gmail.com>
2025-11-08 12:03:24 +0200

    Add annotations to ch9.h
    Signed-off-by: Vadym Hrynchyshyn <vadimgrn@gmail.com>

--- drivers/libdrv/ch9.h
+++ drivers/libdrv/ch9.h
@@ -48,12 +48,12 @@ static_assert(sizeof(USB_ENDPOINT_DESCRIPTOR_AUDIO) == sizeof(USB_ENDPOINT_DESCR
 /**
  * @return 0 to 15
  */
-constexpr auto usb_endpoint_num(const USB_ENDPOINT_DESCRIPTOR &epd)
+constexpr auto usb_endpoint_num(_In_ const USB_ENDPOINT_DESCRIPTOR &epd)
 {
 	return epd.bEndpointAddress & USB_ENDPOINT_ADDRESS_MASK;
 }
 
-constexpr auto usb_endpoint_type(const USB_ENDPOINT_DESCRIPTOR &epd)
+constexpr auto usb_endpoint_type(_In_ const USB_ENDPOINT_DESCRIPTOR &epd)
 {
 	static_assert(USB_ENDPOINT_TYPE_CONTROL == UsbdPipeTypeControl);
 	static_assert(USB_ENDPOINT_TYPE_ISOCHRONOUS == UsbdPipeTypeIsochronous);
@@ -63,12 +63,12 @@ constexpr auto usb_endpoint_type(const USB_ENDPOINT_DESCRIPTOR &epd)
 	return static_cast<USBD_PIPE_TYPE>(epd.bmAttributes & USB_ENDPOINT_TYPE_MASK);
 }
 
-constexpr bool usb_endpoint_dir_in(const USB_ENDPOINT_DESCRIPTOR &epd)
+constexpr bool usb_endpoint_dir_in(_In_ const USB_ENDPOINT_DESCRIPTOR &epd)
 {
 	return USB_ENDPOINT_DIRECTION_IN(epd.bEndpointAddress);
 }
 
-constexpr bool usb_endpoint_dir_out(const USB_ENDPOINT_DESCRIPTOR &epd)
+constexpr bool usb_endpoint_dir_out(_In_ const USB_ENDPOINT_DESCRIPTOR &epd)
 {
 	return USB_ENDPOINT_DIRECTION_OUT(epd.bEndpointAddress);
 }
@@ -77,7 +77,7 @@ constexpr bool usb_endpoint_dir_out(const USB_ENDPOINT_DESCRIPTOR &epd)
  * Default control pipe doesn't have descriptor, but zeroed descriptor 
  * has bEndpointAddress|bmAttributes that match expectations.
  */
-constexpr auto usb_default_control_pipe(const USB_ENDPOINT_DESCRIPTOR &epd)
+constexpr auto usb_default_control_pipe(_In_ const USB_ENDPOINT_DESCRIPTOR &epd)
 {
 	return  epd.bLength == sizeof(epd) &&
 		epd.bDescriptorType == USB_ENDPOINT_DESCRIPTOR_TYPE &&
@@ -88,42 +88,43 @@ constexpr auto usb_default_control_pipe(const USB_ENDPOINT_DESCRIPTOR &epd)
 constexpr USB_ENDPOINT_DESCRIPTOR EP0{ sizeof(EP0), USB_ENDPOINT_DESCRIPTOR_TYPE };
 static_assert(usb_default_control_pipe(EP0));
 
-inline auto operator ==(const USB_COMMON_DESCRIPTOR &a, const USB_COMMON_DESCRIPTOR &b)
+
+inline auto operator ==(_In_ const USB_COMMON_DESCRIPTOR &a, _In_ const USB_COMMON_DESCRIPTOR &b)
 {
 	return a.bLength == b.bLength && RtlEqualMemory(&a, &b, b.bLength);
 }
 
-inline auto operator !=(const USB_COMMON_DESCRIPTOR &a, const USB_COMMON_DESCRIPTOR &b)
+inline auto operator !=(_In_ const USB_COMMON_DESCRIPTOR &a, _In_ const USB_COMMON_DESCRIPTOR &b)
 {
 	return !(a == b);
 }
 
-inline auto operator ==(const USB_DEVICE_DESCRIPTOR &a, const USB_DEVICE_DESCRIPTOR &b)
+inline auto operator ==(_In_ const USB_DEVICE_DESCRIPTOR &a, _In_ const USB_DEVICE_DESCRIPTOR &b)
 {
 	return reinterpret_cast<const USB_COMMON_DESCRIPTOR&>(a) == reinterpret_cast<const USB_COMMON_DESCRIPTOR&>(b);
 }
 
-inline auto operator !=(const USB_DEVICE_DESCRIPTOR &a, const USB_DEVICE_DESCRIPTOR &b)
+inline auto operator !=(_In_ const USB_DEVICE_DESCRIPTOR &a, _In_ const USB_DEVICE_DESCRIPTOR &b)
 {
 	return !(a == b);
 }
 
-inline auto operator ==(const USB_CONFIGURATION_DESCRIPTOR &a, const USB_CONFIGURATION_DESCRIPTOR &b)
+inline auto operator ==(_In_ const USB_CONFIGURATION_DESCRIPTOR &a, _In_ const USB_CONFIGURATION_DESCRIPTOR &b)
 {
 	return a.wTotalLength == b.wTotalLength && RtlEqualMemory(&a, &b, b.wTotalLength);
 }
 
-inline auto operator !=(const USB_CONFIGURATION_DESCRIPTOR &a, const USB_CONFIGURATION_DESCRIPTOR &b)
+inline auto operator !=(_In_ const USB_CONFIGURATION_DESCRIPTOR &a, _In_ const USB_CONFIGURATION_DESCRIPTOR &b)
 {
 	return !(a == b);
 }
 
-inline auto operator ==(const USB_ENDPOINT_DESCRIPTOR &a, const USB_ENDPOINT_DESCRIPTOR &b)
+inline auto operator ==(_In_ const USB_ENDPOINT_DESCRIPTOR &a, _In_ const USB_ENDPOINT_DESCRIPTOR &b)
 {
 	return reinterpret_cast<const USB_COMMON_DESCRIPTOR&>(a) == reinterpret_cast<const USB_COMMON_DESCRIPTOR&>(b);
 }
 
-inline auto operator !=(const USB_ENDPOINT_DESCRIPTOR &a, const USB_ENDPOINT_DESCRIPTOR &b)
+inline auto operator !=(_In_ const USB_ENDPOINT_DESCRIPTOR &a, _In_ const USB_ENDPOINT_DESCRIPTOR &b)
 {
 	return !(a == b);
 }
