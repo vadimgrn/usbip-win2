e971d4bdd24744b7bc73e9804b38a381d68384d8
Vadym Hrynchyshyn <vadimgrn@gmail.com>
2026-09-12 08:48:52 +0300

    ude_filter: Declare setup_packet as constexpr

--- drivers/ude_filter/request.h
+++ drivers/ude_filter/request.h
@@ -23,9 +23,9 @@ namespace impl
  * wIndex: Zero
  * wLength: Zero
  */
-inline const USB_DEFAULT_PIPE_SETUP_PACKET setup_packet =
+inline constexpr USB_DEFAULT_PIPE_SETUP_PACKET setup_packet =
 {
-        .bmRequestType = USB_DIR_IN | USB_TYPE_STANDARD | USB_RECIP_DEVICE,
+        .bmRequestType{.B = static_cast<UCHAR>(USB_DIR_IN | USB_TYPE_STANDARD | USB_RECIP_DEVICE)},
         .bRequest = USB_REQUEST_GET_FIRMWARE_STATUS,
         .wValue{.W = USB_GET_FIRMWARE_ALLOWED_OR_DISALLOWED_STATE},
         .wIndex{.W = MAXUSHORT}, // real request should have zero
