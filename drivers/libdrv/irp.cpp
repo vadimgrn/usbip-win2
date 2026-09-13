66fe9e18a17408e01913b4ffd68627afa4ecc6b1
Vadym Hrynchyshyn <vadimgrn@gmail.com>
2026-09-13 09:22:50 +0300

    libdrv: irp.cpp

--- drivers/libdrv/irp.cpp
+++ drivers/libdrv/irp.cpp
@@ -15,6 +15,11 @@ NTSTATUS libdrv::ForwardIrp(_In_ DEVICE_OBJECT *devobj, _In_ IRP *irp)
 
 /*
  * A caller must complete the IRP after this call.
+ *
+ * IoForwardIrpSynchronously only returns FALSE if no next stack location is available in the IRP.
+ * This means your driver is at the bottom of the device stack, or the IRP was poorly constructed
+ * by the sender. Because the function failed, the IRP was never passed down, and you are
+ * responsible for handling its failure lifecycle.
  */
 _IRQL_requires_same_
 _IRQL_requires_max_(APC_LEVEL)
@@ -26,8 +31,9 @@ PAGED NTSTATUS libdrv::ForwardIrpSynchronously(_In_ DEVICE_OBJECT *devobj, _In_ 
 	auto &status = irp->IoStatus.Status;
 
 	if (!IoForwardIrpSynchronously(devobj, irp)) {
-		status = STATUS_NO_MORE_ENTRIES;
-	}
+                status = STATUS_INVALID_DEVICE_REQUEST;
+                irp->IoStatus.Information = 0;
+        }
 
 	return status;
 }
@@ -39,4 +45,70 @@ NTSTATUS libdrv::CompleteRequest(_In_ IRP *irp, _In_ NTSTATUS status)
 	irp->IoStatus.Status = status;
 	CompleteRequest(irp);
 	return status;
+}
+
+_IRQL_requires_max_(DISPATCH_LEVEL)
+NTSTATUS libdrv::sync_irp::ctor(_In_ CCHAR StackSize, _In_ bool ChargeQuota)
+{
+        if (*this) {
+                return STATUS_ALREADY_INITIALIZED;
+        }
+
+        m_irp = IoAllocateIrp(StackSize, ChargeQuota);
+        if (!m_irp) {
+                return STATUS_INSUFFICIENT_RESOURCES;
+        }
+
+        KeInitializeEvent(&m_event, SynchronizationEvent, false);
+        set_completion_routine();
+
+        return STATUS_SUCCESS;
+}
+
+_IRQL_requires_max_(DISPATCH_LEVEL)
+void libdrv::sync_irp::dtor()
+{
+        if (auto ptr = static_cast<IRP*>(InterlockedExchangePointer(reinterpret_cast<PVOID*>(&m_irp), nullptr))) {
+                IoFreeIrp(ptr);
+        }
+}
+
+_IRQL_requires_max_(DISPATCH_LEVEL)
+void libdrv::sync_irp::reset()
+{
+        if (*this) {
+                IoReuseIrp(m_irp, STATUS_SUCCESS);
+                KeClearEvent(&m_event);
+                set_completion_routine();
+        }
+}
+
+_IRQL_requires_same_
+_IRQL_requires_max_(DISPATCH_LEVEL)
+NTSTATUS libdrv::sync_irp::completion(_In_ DEVICE_OBJECT*, _In_ IRP*, _In_ void *context)
+{
+        auto &self = *static_cast<sync_irp*>(context);
+        KeSetEvent(&self.m_event, IO_NO_INCREMENT, false);
+        return StopCompletion;
+}
+
+_IRQL_requires_max_(APC_LEVEL)
+PAGED NTSTATUS libdrv::sync_irp::wait_for_completion(_Inout_ NTSTATUS &status)
+{
+        PAGED_CODE();
+
+        if (!*this) {
+                return status = STATUS_INVALID_DEVICE_STATE;
+        }
+
+        if (status == STATUS_PENDING) {
+                auto st = KeWaitForSingleObject(&m_event, Executive, KernelMode, false, nullptr);
+                if (st != STATUS_SUCCESS) {
+                        NT_ASSERT(!"sync_irp: KeWaitForSingleObject");
+                        return status = st;
+                }
+                status = m_irp->IoStatus.Status;
+        }
+
+        return status;
 }
--- drivers/libdrv/irp.h
+++ drivers/libdrv/irp.h
@@ -4,46 +4,106 @@
 
 #pragma once
 
-#include <libusbip/generic_handle_ex.h>
 #include "codeseg.h"
+#include <libusbip/generic_handle_ex.h>
 
-namespace usbip
+#include <wdm.h>
+
+namespace libdrv
 {
 
 struct irp_ptr_traits
 {
-        static IRP* invalid() noexcept { return nullptr; }
+        static IRP* invalid() { return nullptr; }
 };
 
 _IRQL_requires_same_
 _IRQL_requires_max_(DISPATCH_LEVEL)
-template<>
 inline void close_handle(_In_ IRP *irp, _In_ irp_ptr_traits)
 {
         IoFreeIrp(irp);
 }
 
-} // namespace usbip
-
-
-namespace libdrv
-{
-
-using usbip::swap;
 using usbip::generic_handle;
-using usbip::irp_ptr_traits;
 
 class irp_ptr : public generic_handle<irp_ptr_traits>
 {
 public:
         using generic_handle<irp_ptr_traits>::generic_handle;
 
+	_IRQL_requires_same_
+	_IRQL_requires_max_(DISPATCH_LEVEL)
         irp_ptr(_In_ CCHAR StackSize, _In_ bool ChargeQuota) :
                 irp_ptr(IoAllocateIrp(StackSize, ChargeQuota)) {}
 
-	auto operator &() const { return get(); }
-	auto operator ->() const { return get(); }
-	auto& operator *() const { return *get(); }
+	_IRQL_requires_same_
+	_IRQL_requires_max_(DISPATCH_LEVEL)
+	constexpr auto operator ->(this auto&& self)
+	{
+		NT_ASSERT(self.get());
+		return self.get();
+	}
+
+	_IRQL_requires_same_
+	_IRQL_requires_max_(DISPATCH_LEVEL)
+	constexpr auto& operator *(this auto&& self)
+	{
+		NT_ASSERT(self.get());
+		return *self.get();
+	}
+};
+
+
+class sync_irp
+{
+public:
+        _IRQL_requires_max_(DISPATCH_LEVEL)
+        sync_irp() { ctor(); } // works for allocations on stack
+
+        _IRQL_requires_max_(DISPATCH_LEVEL)
+        ~sync_irp() { dtor(); }
+
+        _IRQL_requires_max_(DISPATCH_LEVEL)
+        NTSTATUS ctor(_In_ CCHAR StackSize = 1, _In_ bool ChargeQuota = false);
+
+        _IRQL_requires_max_(DISPATCH_LEVEL)
+        void dtor();
+
+        sync_irp(_In_ const sync_irp&) = delete;
+        sync_irp& operator=(_In_ const sync_irp&) = delete;
+
+        constexpr explicit operator bool(this auto&& self) { return self.m_irp; }
+        constexpr auto operator !(this auto&& self) { return !self.m_irp; }
+
+        constexpr auto get(this auto&& self) { return self.m_irp; }
+        constexpr auto operator ->(this auto&& self)
+        {
+                NT_ASSERT(self.m_irp);
+                return self.m_irp;
+        }
+        constexpr auto& operator *(this auto&& self) { NT_ASSERT(self.m_irp); return *self.m_irp; }
+
+        _IRQL_requires_max_(APC_LEVEL)
+        PAGED NTSTATUS wait_for_completion(_Inout_ NTSTATUS &status);
+
+        _IRQL_requires_max_(DISPATCH_LEVEL)
+        void reset();
+
+private:
+        IRP *m_irp{};
+        KEVENT m_event{};
+
+        _IRQL_requires_same_
+        _IRQL_requires_max_(DISPATCH_LEVEL)
+        static NTSTATUS completion(_In_ DEVICE_OBJECT*, _In_ IRP*, _In_ void *context);
+
+        _IRQL_requires_same_
+        _IRQL_requires_max_(DISPATCH_LEVEL)
+        void set_completion_routine()
+        {
+                NT_ASSERT(m_irp);
+                IoSetCompletionRoutine(m_irp, completion, this, true, true, true);
+        }
 };
 
 
@@ -54,19 +114,28 @@ constexpr auto list_entry(_In_ IRP *irp)
 	return &irp->Tail.Overlay.ListEntry;
 }
 
+/*
+ * @param entry must be Tail.Overlay.ListEntry.
+ */
 _IRQL_requires_same_
 _IRQL_requires_max_(DISPATCH_LEVEL)
 inline auto get_irp(_In_ LIST_ENTRY *entry)
 {
-	return CONTAINING_RECORD(entry, IRP, Tail.Overlay.ListEntry);
+        NT_ASSERT(entry);
+        auto irp = CONTAINING_RECORD(entry, IRP, Tail.Overlay.ListEntry);
+
+        NT_ASSERT(irp->Type == IO_TYPE_IRP);
+        NT_ASSERT(irp->Size >= sizeof(IRP));
+
+        return irp;
 }
 
 /*
  * IRP.Tail.Overlay.DriverContext[] must not be used.
  */
+template<typename T>
 _IRQL_requires_same_
 _IRQL_requires_max_(DISPATCH_LEVEL)
-template<typename T>
 inline auto& get_params_others(_In_ IO_STACK_LOCATION *loc)
 {
         NT_ASSERT(loc);
