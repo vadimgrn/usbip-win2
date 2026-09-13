6dc463ceaeb519bd7e1b40b34390b60426579b8f
Vadym Hrynchyshyn <vadimgrn@gmail.com>
2026-09-13 09:27:27 +0300

    ude_filter: pnp.cpp

--- drivers/ude_filter/pnp.h
+++ drivers/ude_filter/pnp.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 2022-2025 Vadym Hrynchyshyn <vadimgrn@gmail.com>
+ * Copyright (c) 2022-2026 Vadym Hrynchyshyn <vadimgrn@gmail.com>
  */
 
 #pragma once
 
-#include <libdrv\codeseg.h>
+#include <libdrv/codeseg.h>
 
 namespace usbip
 {
