4a97876e936b2331cbe28369abaea47e4a0ddf1d
Vadym Hrynchyshyn <vadimgrn@gmail.com>
2026-09-13 09:31:41 +0300

    libusbip: proto_op.cpp

--- userspace/libusbip/src/proto_op.cpp
+++ userspace/libusbip/src/proto_op.cpp
@@ -1,8 +1,8 @@
-﻿/*
- * Copyright (c) 2022-2025 Vadym Hrynchyshyn <vadimgrn@gmail.com>
+/*
+ * Copyright (c) 2022-2026 Vadym Hrynchyshyn <vadimgrn@gmail.com>
  */
 
-#include <usbip\proto_op.h>
+#include <usbip/proto_op.h>
 #include <intrin.h>
 
 namespace
