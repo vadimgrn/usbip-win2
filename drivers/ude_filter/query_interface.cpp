bc86c8cbf9ce7e446a3dd2ef78217f9c4f98d529
Vadym Hrynchyshyn <vadimgrn@gmail.com>
2026-09-13 09:27:18 +0300

    ude_filter: query_interface.cpp

--- drivers/ude_filter/query_interface.cpp
+++ drivers/ude_filter/query_interface.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2025 Vadym Hrynchyshyn <vadimgrn@gmail.com>
+ * Copyright (c) 2023-2026 Vadym Hrynchyshyn <vadimgrn@gmail.com>
  */
 
 #include "query_interface.h"
@@ -23,9 +23,8 @@ _Must_inspect_result_ NTSTATUS USB_BUSIFFN QueryBusTime(
 	_Out_opt_ ULONG *CurrentUsbFrame)
 {
 	if (CurrentUsbFrame) {
-		static ULONG dummy;
-		*CurrentUsbFrame = ++dummy; // zero is OK too
-		// TraceDbg("%lu", *CurrentUsbFrame); // too often
+		// 1 frame = 1ms = 10,000 * 100ns units
+		*CurrentUsbFrame = static_cast<ULONG>(KeQueryInterruptTime() / 10'000);
 	}
 
 	return STATUS_SUCCESS;
@@ -42,15 +41,15 @@ _Function_class_(PUSB_BUSIFFN_QUERY_BUS_TIME_EX)
 _IRQL_requires_same_
 _IRQL_requires_max_(DISPATCH_LEVEL)
 _Must_inspect_result_ NTSTATUS USB_BUSIFFN QueryBusTimeEx(
-	_In_opt_ PVOID BusContext,
-	_Out_opt_ PULONG HighSpeedFrameCounter)
+	_In_opt_ void *,
+	_Out_opt_ ULONG *HighSpeedFrameCounter)
 {
-	auto st = QueryBusTime(BusContext, HighSpeedFrameCounter);
-	if (NT_SUCCESS(st) && HighSpeedFrameCounter) {
-		*HighSpeedFrameCounter <<= 3;
-		// TraceDbg("%lu", **HighSpeedFrameCounter); // too often
+	if (HighSpeedFrameCounter) {
+		// 1 micro-frame = 125us = 1,250 * 100ns units (8 micro-frames per 1ms frame)
+		*HighSpeedFrameCounter = static_cast<ULONG>(KeQueryInterruptTime() / 1'250);
 	}
-	return st;
+
+	return STATUS_SUCCESS;
 }
 
 } // namespace
@@ -61,13 +60,26 @@ _Must_inspect_result_ NTSTATUS USB_BUSIFFN QueryBusTimeEx(
  */
 _IRQL_requires_same_
 _IRQL_requires_(PASSIVE_LEVEL)
-PAGED void usbip::query_interface(_Inout_ filter_ext &, _Inout_ _USB_BUS_INTERFACE_USBDI_V3 &r)
+PAGED NTSTATUS usbip::query_interface(_Inout_ [[maybe_unused]] filter_ext &fltr, _Inout_ _USB_BUS_INTERFACE_USBDI_V3 &r)
 {
 	PAGED_CODE();
 
+	const auto end = offsetof(_USB_BUS_INTERFACE_USBDI_V3, QueryBusTime) + sizeof(r.QueryBusTime);
+	const auto ex_end = offsetof(_USB_BUS_INTERFACE_USBDI_V3, QueryBusTimeEx) + sizeof(r.QueryBusTimeEx);
+
+	if (r.Size < end) {
+		return STATUS_INVALID_PARAMETER;
+	}
+
 	switch (ULONG dummy; r.Version) {
 	case USB_BUSIF_USBDI_VERSION_3:
-		if (auto st = r.QueryBusTimeEx(r.BusContext, &dummy); NT_ERROR(st)) {
+		if (r.Size < ex_end) {
+			return STATUS_INVALID_PARAMETER;
+		}
+		if (!r.QueryBusTimeEx) {
+			TraceDbg("QueryBusTimeEx is NULL, substituted");
+			r.QueryBusTimeEx = QueryBusTimeEx;
+		} else if (auto st = r.QueryBusTimeEx(r.BusContext, &dummy); NT_ERROR(st)) {
 			TraceDbg("QueryBusTimeEx -> %!STATUS!, substituted", st);
 			r.QueryBusTimeEx = QueryBusTimeEx;
 		}
@@ -75,12 +87,18 @@ PAGED void usbip::query_interface(_Inout_ filter_ext &, _Inout_ _USB_BUS_INTERFA
 	case USB_BUSIF_USBDI_VERSION_2:
 	case USB_BUSIF_USBDI_VERSION_1:
 	case USB_BUSIF_USBDI_VERSION_0:
-		if (auto st = r.QueryBusTime(r.BusContext, &dummy); NT_ERROR(st)) {
+		if (!r.QueryBusTime) {
+			TraceDbg("QueryBusTime is NULL, substituted");
+			r.QueryBusTime = QueryBusTime;
+		} else if (auto st = r.QueryBusTime(r.BusContext, &dummy); NT_ERROR(st)) {
 			TraceDbg("QueryBusTime -> %!STATUS!, substituted", st);
 			r.QueryBusTime = QueryBusTime;
 		}
 		break;
 	default:
-		Trace(TRACE_LEVEL_ERROR, "Unexpected USB_BUSIF_USBDI_VERSION_%lu", r.Version);
+		Trace(TRACE_LEVEL_WARNING, "Unexpected USB_BUSIF_USBDI_VERSION_%lu", r.Version);
+		break;
 	}
+
+	return STATUS_SUCCESS;
 }
