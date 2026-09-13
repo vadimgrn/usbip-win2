2a2d4ebc36df5494e7f12e19a195399fc7200b63
Vadym Hrynchyshyn <vadimgrn@gmail.com>
2026-09-13 09:34:01 +0300

    usbip: list.cpp

--- userspace/usbip/list.cpp
+++ userspace/usbip/list.cpp
@@ -4,11 +4,11 @@
 
 #include "usbip.h"
 #include "strings.h"
+#include "log.h"
 
 #include <libusbip/vhci.h>
 #include <libusbip/persistent.h>
 
-#include <spdlog/spdlog.h>
 #include <print>
 
 namespace
@@ -60,42 +60,44 @@ void on_interface(int, const usb_device &d, int idx, const usb_interface &r)
 
 auto list_persistent_devices()
 {
-	if (auto dev = vhci::open(); !dev) {
-		spdlog::error(GetLastErrorMsg());
-                return false;
-        } else if (auto v = vhci::get_persistent(dev.get()); !v) {
-		spdlog::error(GetLastErrorMsg());
-                return false;
-        } else for (auto &i: *v) {
-                auto &loc = i.location;
-                std::println("{}:{}/{}, serial '{}', mode:{}, once:{}",
-                        loc.hostname, loc.service, loc.busid, i.serial,
-                        to_string(i.recv_mode), i.once);
-        }
+	auto dev = vhci::open();
+	if (!dev) {
+		log::error(get_last_error_msg());
+		return false;
+	}
 
-        return true;
+	auto v = vhci::get_persistent(dev.get());
+	if (!v) {
+		log::error(get_last_error_msg());
+		return false;
+	}
+
+	for (const auto &i: *v) {
+		std::println("{}", i);
+	}
+
+	return true;
 }
 
 } // namespace
 
 
-bool usbip::cmd_list(void *p)
+bool usbip::cmd_list(const list_args &args)
 {
-	auto &args = *reinterpret_cast<list_args*>(p);
 	if (args.persistent) {
 		return list_persistent_devices();
 	}
 
 	auto sock = connect(args.remote.c_str(), global_args.tcp_port.c_str());
 	if (!sock) {
-		spdlog::error(GetLastErrorMsg());
+		log::error(get_last_error_msg());
 		return false;
 	}
 
-	spdlog::debug("connected to {}:{}", args.remote, global_args.tcp_port);
+	log::debug("connected to {}:{}", args.remote, global_args.tcp_port);
 
 	if (!enum_exportable_devices(sock.get(), on_device, on_interface, on_device_count)) {
-		spdlog::error(GetLastErrorMsg());
+		log::error(get_last_error_msg());
 		return false;
 	}
 
