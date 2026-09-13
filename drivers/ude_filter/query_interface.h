0f6b49dd4964a46cf088db4eb7071b21f2a0ad1b
Vadym Hrynchyshyn <vadimgrn@gmail.com>
2026-09-13 09:27:18 +0300

    ude_filter: query_interface.cpp

--- drivers/ude_filter/query_interface.h
+++ drivers/ude_filter/query_interface.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 2023-2025 Vadym Hrynchyshyn <vadimgrn@gmail.com>
+ * Copyright (c) 2023-2026 Vadym Hrynchyshyn <vadimgrn@gmail.com>
  */
 
 #pragma once
 
-#include <libdrv\codeseg.h>
+#include <libdrv/codeseg.h>
 
 struct _USB_BUS_INTERFACE_USBDI_V3;
 
@@ -15,6 +15,6 @@ struct filter_ext;
 
 _IRQL_requires_same_
 _IRQL_requires_(PASSIVE_LEVEL)
-PAGED void query_interface(_Inout_ filter_ext &fltr, _Inout_ _USB_BUS_INTERFACE_USBDI_V3 &v3);
+PAGED NTSTATUS query_interface(_Inout_ [[maybe_unused]] filter_ext &fltr, _Inout_ _USB_BUS_INTERFACE_USBDI_V3 &v3);
 
 } // namespace usbip
